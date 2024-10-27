6aea4b90f7ee61a1e51567303ada917855c289ae
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-10-28 02:04:52 +0530

    Added Movie.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Step 1: Create a Movie instance
+        Movie movie1 = new Movie("Inception", "Sci-Fi", 148, 8.8);
+        movie1.getDetails();  // Expected output: Title: Inception, Genre: Sci-Fi, Duration: 148 mins, Rating: 8.8
+    }
+}
