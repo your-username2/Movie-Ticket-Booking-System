d4266e309f7d6a11ff91f9eadd7270c41b01a295
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-10-28 02:02:22 +0530

    revert to start to do from scratch

--- Payment/Payment.java
+++
@@ -1,9 +0,0 @@
-public abstract class Payment {
-    protected double amount;
-
-    public Payment(double amount) {
-        this.amount = amount;
-    }
-
-    public abstract void processPayment();
-}
